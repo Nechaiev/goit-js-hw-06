847809f624c0214626209505daf9470726c3d578
Stanislav <stanislav.nechaev@gmail.com>
2023-01-23 19:46:32 +0200

    exercise 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,28 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients')
+
+const liArray = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+
+list.append(...liArray)
+
+
+// const list = document.querySelector('#ingredients')
+// const liArray = []
+
+// ingredients.forEach(ingredient => {
+//   const item = document.createElement('li')
+//   item.className = 'item'
+//   item.textContent = ingredient
+//   liArray.push(item)
+// })
+
+// list.append(...liArray)
+
