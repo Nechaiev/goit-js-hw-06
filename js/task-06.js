d8a631d15f1aa32c6d5be9ac53cd5140b2632ad6
Stanislav <stanislav.nechaev@gmail.com>
2023-01-24 21:57:28 +0200

    exercise 6

---
+++ js/task-06.js
@@ -0,0 +1,28 @@
+const textInput = document.querySelector("#validation-input");
+textInput.addEventListener("blur", onBlur);
+function onBlur(e) {
+  const input1 = e.currentTarget;
+  const inputLength = Number(input1.dataset.length);
+  if (input1.value.length === inputLength) {
+    input1.classList.remove("invalid");
+    input1.classList.add("valid");
+  } else {
+    input1.classList.remove("valid");
+    input1.classList.add("invalid");
+  }
+}
+
+// const textInput = document.querySelector('#validation-input');
+// textInput.addEventListener('blur', event => {
+//   if (event.target.value.length == textInput.getAttribute('data-length')) {
+//     textInput.classList.add('valid')
+//     if (textInput.classList.contains('invalid')) {
+//       textInput.classList.remove('invalid')
+//     }
+//   } else {
+//     if (textInput.classList.contains('valid')) {
+//       textInput.classList.remove('valid')
+//     }
+//     textInput.classList.add('invalid')
+//   }
+// })
