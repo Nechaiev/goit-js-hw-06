4a3901a765963651e73846dcd5d53a4c51250fb1
Stanislav <stanislav.nechaev@gmail.com>
2023-01-25 23:26:35 +0200

    fix

--- js/task-05.js
+++ js/task-05.js
@@ -2,10 +2,27 @@ const textInput = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 textInput.addEventListener('input', event => {
-  output.textContent = event.target.value
-
-  if (event.target.value === '') {
-    output.textContent = 'Anonymous'
-  }
+  output.textContent = event.target.value !== '' ? event.target.value : 'Anonymous'
 })
 
+
+// const textInput = document.querySelector('#name-input');
+// const output = document.querySelector('#name-output');
+
+// textInput.addEventListener('input', event => {
+//   output.textContent = event.target.value
+//   if (event.target.value === '') {
+//     output.textContent = 'Anonymous'
+//   }
+// })
+
+
+// const textInput = document.querySelector('#name-input');
+// const output = document.querySelector('#name-output');
+
+// textInput.addEventListener('input', event => {
+//   if (event.target.value === '') {
+//     output.textContent = 'Anonymous'
+//   }
+//   output.textContent = event.target.value
+// })
